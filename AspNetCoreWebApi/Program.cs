032aaf946af5f75fb29b1631a6285c72fd1499b2
Tie Antono <tietieantono@gmail.com>
2023-03-14 22:05:58 +0700

    Added CORS policy

--- AspNetCoreWebApi/Program.cs
+++ AspNetCoreWebApi/Program.cs
@@ -39,6 +39,18 @@ builder.Services.AddAuthentication("Bearer")
         };
     });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "localhost",
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000")
+                          // Bypass all other rules for convenient. Do not do this in real project scenarios.
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                      });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -47,6 +59,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("localhost");
 
 app.UseAuthentication();
 app.UseAuthorization();
